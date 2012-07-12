8aab0c39e92185babbac71980a6ad8f8aaa68e3c
Tomas <tomas@libal.eu>
2012-07-12 07:58:28 -0400

    formbuilder.php file added

---
+++ formbuilder.php
@@ -0,0 +1,10 @@
+<?php
+#
+# This file can be included in other projects and will bring in the onlineformsmarker
+# functionality into the projects.
+#
+
+// Load the environment settings
+require_once 'env.php';
+// Add a class loader
+require_once 'classLoader.php';
