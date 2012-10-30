2afa75b334f2296cc6b8bac8671e4e8431a8b465
Tomas <tomas@libal.eu>
2012-10-30 17:06:45 -0400

    updated parsing of the forms, now using the processor class

--- reader/reader.php
+++ reader/reader.php
@@ -15,22 +15,7 @@ class reader
 {
     private $data;
     private $dataOk;
-    
-    /**
-    * Filters out all non-alphabet characters from text
-    *
-    * @param string $string String from which the alphabet characters will be filtered out
-    * @return string
-    * @access private
-    */
-    private function filterOutText($string)
-    {
-        if(preg_match("/([\w]+)/i", $string, $match)) {
-            return $match[0];
-        }else {
-            return null;
-        }  
-    }
+    private $processor;
     
     public function __construct()
     {
@@ -55,68 +40,21 @@ class reader
     */
     public function parse()
     {
-        $elements = array();
-        
-        // FORM TITLE
-        $titleRE = "/([\w ]+).([\+]{1,})/is";
-        if(preg_match($titleRE, $this->data, $matches)) {
-            $title = new title();
-            $title->value = $matches[1];
-            
-            $elements[] = $title;
-        }
-        
-        // line breakings
-        $breaksRE = "/(\s]+\-\-[\s]+)/is";
-        $breaksRE = "/(\-\-)/is";
-        if(preg_match($breaksRE, $this->data, $matches)) {
-            $break = new linebreak();
-            $elements[] = $break;
-        }
-        
-        // modifiers i; g (global) modifier for multiple matching is replaced by preg_match_all function usage
-        $inputsRE = "/\(input(|[:\w]+)(|#[\w]+)\)(|@@[\w]+@@)([\w ()]+)/i";
-        preg_match_all($inputsRE, $this->data, $matches, PREG_SET_ORDER);
-        foreach($matches as $val) {
-            $tmp = new input();
-            $tmp->label = $val[4];
+        $RE = array("sectioncontrolbox" => "/\(sectioncontrolbox(|[:\w]+)\)/is",
+            "clearleft" => "/\(clearleft\)/is",
+            "title" => "/([\w ]+).([\+]+)/is",
+            "selects" => "/\(select(|box)(|[:\w]+)(|[\[\w\d -\]]+)\)(|[\w \.()]+)/is",
+            "breaks" => "/([ ]\-\-[ ])/is",
+            "inputs" => "/\(input(|[:\w]+)(|#[\w]+)(|\[[\w]+\])\)(|@@[\w]+@@)([\w \.()\?]+)/i",
+            "textareas" => "/\(textarea(|#[\w]+)\)([\w ]{0,})/i",
+            "buttons" => "/\(button(|[:\w]+)(|#[\w]+)\)([\w ]{0,})/i"
+            );
 
-            $tmp->value = $val[3];
-            
-            $tmp->id = $this->filterOutText($val[2]);
-            $tmp->inputType = $this->filterOutText($val[1]);
-            
-            $elements[] = $tmp;        
+        foreach($RE as $key => $val) {
+            $this->data = preg_replace_callback($val, array($this->processor, $key), $this->data);
         }
         
-        // !!!
-        $textareasRE = "/\(textarea(|#[\w]+)\)([\w ]{0,})/i";
-        preg_match_all($textareasRE, $this->data, $matches, PREG_SET_ORDER);
-        foreach($matches as $val) {
-            $tmp = new textarea();
-            
-            $tmp->caption = $val[2];
-
-            $tmp->id = $this->filterOutText($val[1]);
-            $tmp->name = $this->filterOutText($val[1]);
-            
-            
-            $elements[] = $tmp;            
-        }
-        
-        $buttonsRE = "/\(button(|[:\w]+)(|#[\w]+)\)([\w ]{0,})/i";
-        preg_match_all($buttonsRE, $this->data, $matches, PREG_SET_ORDER);
-        foreach($matches as $val) {
-            $tmp = new button();
-            $tmp->value = $val[3];
-            $tmp->type = $this->filterOutText($val[1]);
-            
-            $tmp->id = $this->filterOutText($val[2]);
-            
-            $elements[] = $tmp;            
-        }
-        
-        return $elements;
+        return $this->data;
     }
      
     /**
