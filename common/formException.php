546aee614c9f0d5ff84a5974dc7943f0b478e39b
Tomas <tomas@libal.eu>
2012-11-01 16:27:28 -0400

    changed the default message of formException and added a custom construct method (still calling the parent)

--- common/formException.php
+++ common/formException.php
@@ -10,5 +10,11 @@
 */
 class formException extends Exception
 {
+    protected $message = "unknown formException";
     
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        
+        // to make sure everything is assigned properly
+        parent::__construct($message, $code, $previous);
+    }
 }
