10ee54d0fc01d62d717f06853ffe424d51e29463
Tom <tomas@libal.eu>
2013-03-16 11:54:36 +0100

    renamed formbuilder.php

---
+++ onlineformsmarker.php
@@ -0,0 +1,10 @@
+<?php
+#
+# Include this file into other projects and it will bring in the onlineformsmarker
+# functionality into the projects.
+#
+
+// Load the environment settings
+require_once 'env.php';
+// Add a class loader
+require_once 'classLoader.php';
