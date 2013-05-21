3a629a3c930739b3e82e0f4c03835ba5bc4f082d
Tomas Libal <tomas@libal.eu>
2013-05-21 19:30:43 +0100

    example of a standalone form page

---
+++ Examples/example02.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../App/Form.php';
+
+$testForm = "
+TEST FORM
+++++++++++++++++++++++++++
+
+(input)Username
+(input)Email
+(input:password)Your password
+
+(button)Register me!
+";
+
+$myForm = new \OFM\App\Form();
+$myForm->standalone = true;
+$myForm->action = 'sendTo.php';
+$myForm->loadString($testForm);
+echo $myForm;
+?>
