3184d97e563c494a92be53d08de5de4bfb97067c
Tomas Libal <tomas@libal.eu>
2014-05-11 16:48:44 +0100

    implements ILexer
    At the moment assumes tokens to be non-whitespace characters delimited by space,tab or new line feeds

---
+++ App/FormLexer.php
@@ -0,0 +1,40 @@
+<?php
+namespace OFM\App;
+
+class FormLexer implements ILexer
+{
+    private $num_tokens = 0;
+
+    public function tokenize($str)
+    {
+	$ascii = utf8_decode($str);
+	$len   = strlen($ascii);
+	
+	$buf = '';
+	$tok_started = 0;
+	$tokens = array();
+
+	for($i = 0; $i < $len; $i++) {
+	       // if not a white-space char:
+	       $chr = ord($ascii[$i]);
+	       if(($chr>47&&$chr<58)||($chr>64&&$chr<91)||($chr>94&&$chr<123)||($chr>44&&$chr<48)) {
+	       $buf .= $ascii[$i];
+	       if($tok_started==0)$tok_started=1;
+} else if(($chr==32||$chr==10||$chr==9)&&$tok_started==1) {
+  array_push($tokens,$buf);
+  $buf = '';
+  $tok_started=0;
+  $this->num_tokens++;
+}
+	       echo $ascii[$i]."<br>\n";
+	}
+	// consider anything in buffer a token
+	if(strlen($buf)>0){
+	    array_push($tokens,$buf);
+	}
+
+	return $tokens;
+    }
+}
+
+?>
