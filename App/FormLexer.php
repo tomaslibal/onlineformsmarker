137a41c0d8822424fe706921d7aa80f1298f8f9a
Tomas Libal <tomas@libal.eu>
2014-05-11 17:06:56 +0100

    closing parenthesses closes a token

--- App/FormLexer.php
+++ App/FormLexer.php
@@ -1,7 +1,8 @@
 <?php
 namespace OFM\App;
+require_once OFMHOME."/I/ILexer.php";
 
-class FormLexer implements ILexer
+class FormLexer implements \OFM\Interfaces\ILexer
 {
     private $num_tokens = 0;
 
@@ -11,7 +12,8 @@ class FormLexer implements ILexer
 	$len   = strlen($ascii);
 	
 	$buf = '';
-	$tok_started = 0;
+    $tok_started = 0; // any non-white-space
+    $el_started = 0; // (input)
 	$tokens = array();
 
 	for($i = 0; $i < $len; $i++) {
@@ -20,13 +22,12 @@ class FormLexer implements ILexer
 	       if(($chr>47&&$chr<58)||($chr>64&&$chr<91)||($chr>94&&$chr<123)||($chr>44&&$chr<48)) {
 	       $buf .= $ascii[$i];
 	       if($tok_started==0)$tok_started=1;
-} else if(($chr==32||$chr==10||$chr==9)&&$tok_started==1) {
+} else if(($chr==32||$chr==10||$chr==9||$chr==41)&&$tok_started==1) {
   array_push($tokens,$buf);
   $buf = '';
   $tok_started=0;
   $this->num_tokens++;
 }
-	       echo $ascii[$i]."<br>\n";
 	}
 	// consider anything in buffer a token
 	if(strlen($buf)>0){
