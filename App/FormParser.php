c82404b7155657b87904b1257a6610009cf9ece8
Tomas Libal <tomas@libal.eu>
2014-05-19 20:28:30 +0100

    parsing now 'textarea' and 'title'

--- App/FormParser.php
+++ App/FormParser.php
@@ -4,6 +4,8 @@ require_once OFMHOME."/I/IParser.php";
 
 require_once OFMHOME."/Components/Input.php";
 require_once OFMHOME."/Components/Button.php";
+require_once OFMHOME."/Components/Textarea.php";
+require_once OFMHOME."/Components/Title.php";
 
 class FormParser implements \OFM\Interfaces\IParser
 {
@@ -30,6 +32,22 @@ class FormParser implements \OFM\Interfaces\IParser
                     $t = array_shift($obj->tokens);
                     if(substr($t, 0, 1)=="#") $tmp->name = substr($t, 1);
                     else $tmp->label .= $t.' ';
+                }
+                array_push($this->elems, $tmp);
+                break;
+            case 'textarea':
+                $tmp = new \OFM\Components\Textarea();
+                while(count($obj->tokens)>0) {
+                    $t = array_shift($obj->tokens);
+                    if(substr($t, 0, 1)=="#") $tmp->name = substr($t, 1);
+                    else $tmp->data .= $t;
+                }
+                array_push($this->elems, $tmp);
+                break;
+            case 'title':
+                $tmp = new \OFM\Components\Title();
+                while(count($obj->tokens)>0) {
+                    $tmp->value .= array_shift($obj->tokens) . ' ';
                 }
                 array_push($this->elems, $tmp);
                 break;
