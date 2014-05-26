124fe6a51ec4770d2f13b2acd395962d2cf25f6d
Tomas Libal <tomas@libal.eu>
2014-05-26 21:35:46 +0200

    Selectbox parsing

--- App/FormParser.php
+++ App/FormParser.php
@@ -6,6 +6,7 @@ require_once OFMHOME."/Components/Input.php";
 require_once OFMHOME."/Components/Button.php";
 require_once OFMHOME."/Components/Textarea.php";
 require_once OFMHOME."/Components/Title.php";
+require_once OFMHOME."/Components/Selectbox.php";
 
 class FormParser implements \OFM\Interfaces\IParser
 {
@@ -42,7 +43,9 @@ class FormParser implements \OFM\Interfaces\IParser
                 while(count($obj->tokens)>0) {
                     $t = array_shift($obj->tokens);
                     if(substr($t, 0, 1)=="#") $tmp->name = substr($t, 1);
-                    else $tmp->data .= $t;
+                    else if(substr($t, 0, 2)=="@@") $tmp->data .= substr($t, 2, strlen($t)-2).' ';
+                    else if(substr($t, -2)=="@@") $tmp->data .= substr($t, 0, strlen($t)-2).' ';
+                    else $tmp->data .= $t.' ';
                 }
                 array_push($this->elems, $tmp);
                 break;
@@ -53,11 +56,24 @@ class FormParser implements \OFM\Interfaces\IParser
                 }
                 array_push($this->elems, $tmp);
                 break;
+            case 'selectbox':
+                $tmp = new \OFM\Components\Selectbox();
+                while(count($obj->tokens)>0){
+                    $t = array_shift($obj->tokens);
+                    if(substr($t, 0, 1)=="[") {
+                        $arr=explode("=", substr($t, 1, strlen($t)-2)); // -2 because the str is already 1 char shorter from the start
+                        array_push($tmp->values, $arr);
+                        //print_r($arr);
+                    }
+                }
+                array_push($this->elems, $tmp);
+                break;
             case 'button':
                 $tmp = new \OFM\Components\Button();
                 while(count($obj->tokens)>0) {
                     $t = array_shift($obj->tokens);
                     if(substr($t, 0, 1)=="#") $tmp->name = substr($t, 1);
+                    else if(substr($t, 0, 1)==":") $tmp->type = substr($t, 1);
                     else $tmp->value .= $t.' ';
                 }
                 array_push($this->elems, $tmp);
