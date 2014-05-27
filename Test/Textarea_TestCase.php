7e463f6cafb967385f03372c5a3928fb52bd4bfd
Tomas Libal <tomas@libal.eu>
2014-05-27 21:11:58 +0200

    basic Textare component test case

--- Test/Textarea_TestCase.php
+++ Test/Textarea_TestCase.php
@@ -21,6 +21,34 @@ class Textarea_TestCase extends \OFM\Test\TestCase
 
 	public function run()
 	{
+
+		// default
+		$str = "<textarea></textarea>\n";
+		$out = $this->obj->__toString();
+		$this->assertEquals(0, strcmp($str, $out), "Default options");
+
+		// data
+		$str = "<textarea>Sample data</textarea>\n";
+		$this->obj = $this->renewObj();
+		$this->obj->data = "Sample data";
+		$out = $this->obj->__toString();
+		$this->assertEquals(0, strcmp($str, $out), "Data value");
+
+		// label rendering
+		$str = "<label for=\"some_name\">Test caption</label><textarea name=\"some_name\"></textarea>\n";
+		$this->obj = $this->renewObj();
+		$this->obj->name = "some_name";
+		$this->obj->caption = "Test caption";
+		$out = $this->obj->__toString();
+		$this->assertEquals(0, strcmp($str, $out), "Label rendering");
+
+		// id
+		$str = "<textarea id=\"myId\"></textarea>\n";
+		$this->obj = $this->renewObj();
+		$this->obj->id = "myId";
+		$out = $this->obj->__toString();
+		$this->assertEquals(0, strcmp($str, $out), "ID Attribute");
+
 		return $this->getInfo();
 	}
 }
