7c622dc579e7c7678c7da52fd4a2dc9e22d30ee5
Tomas Libal <tomas@libal.eu>
2014-06-05 20:36:56 +0200

    added tests for unicode (and one test for multiple tokens in one string)

--- Test/FormLexer_TestCase.php
+++ Test/FormLexer_TestCase.php
@@ -33,15 +33,26 @@ class FormLexer_TestCase extends \OFM\Test\TestCase
 		$this->assertEquals(count($tmp[0]->tokens), 1, "One token in the object should be present ", __FILE__, __LINE__);
 		$this->assertEquals(strcmp('0123456789', $tmp[0]->tokens[0]), 0, "The token should have value of '0123456789'");
 
+		// multiple tokens
+		$tmp = $this->obj->tokenize('(button here is some button)');
+		$this->assertEquals(count($tmp[0]->tokens), 4, "4 tokens should be present", __FILE__, __LINE__);
+
 		// test that letters a-z are being properly tokenized
 		$tmp = $this->obj->tokenize('(input abcdefghjklimnopqrstuvwxyz)');
 		$this->assertEquals(count($tmp[0]->tokens), 1, "One token in the object should be present");
-		$this->assertEquals(strcmp('abcdefghjklimnopqrstuvwxyz', $tmp[0]->tokens[0]), 0, "The token should have value of 'abcdefghjklimnopqrstuvwxyz'");		
+		$this->assertEquals(strcmp('abcdefghjklimnopqrstuvwxyz', $tmp[0]->tokens[0]), 0, "The token should have value of 'abcdefghjklimnopqrstuvwxyz'");	
 
 		// test the letters A-Z are being properly tokenized
 		$tmp = $this->obj->tokenize('(input ABCDEFGHJKLIMNOPQRSTUVWXYZ)');
 		$this->assertEquals(count($tmp[0]->tokens), 1, "One token in the object should be present");
-		$this->assertEquals(strcmp('ABCDEFGHJKLIMNOPQRSTUVWXYZ', $tmp[0]->tokens[0]), 0, "The token should have value of 'abcdefghjklimnopqrstuvwxyz'");				
+		$this->assertEquals(strcmp('ABCDEFGHJKLIMNOPQRSTUVWXYZ', $tmp[0]->tokens[0]), 0, "The token should have value of 'ABCDEFGHJKLIMNOPQRSTUVWXYZ'");
+
+		// test unicode
+		$tmp = $this->obj->tokenize('(textarea âūtóçabcdef€ hello there)');
+		$this->assertEquals(strcmp('âūtóçabcdef€', $tmp[0]->tokens[0]),0, "unicode tokenizer test");
+		$this->assertEquals(count($tmp[0]->tokens), 3, "3 tokens total should be there");
+
+
 
 		// return the info array
 		return $this->getInfo();
